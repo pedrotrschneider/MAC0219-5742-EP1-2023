6dab50963319a0466aac7b376d313e6b9ee6468d
Pedro Tonini Rosenberg Schneider <pedrotrschneider@gmail.com>
2023-07-18 20:59:51 -0300

    ✨ Paralellize the simulation using Open MP

--- src/lga_omp.c
+++ src/lga_omp.c
@@ -1,45 +1,67 @@
+#include <omp.h>
+
 #include "lga_base.h"
 #include "lga_omp.h"
 
-static byte get_next_cell(int i, int j, byte *grid_in, int grid_size) {
+static byte
+get_next_cell (int i, int j, byte *grid_in, int grid_size)
+{
     byte next_cell = EMPTY;
 
-    for (int dir = 0; dir < NUM_DIRECTIONS; dir++) {
-        int rev_dir = (dir + NUM_DIRECTIONS/2) % NUM_DIRECTIONS;
+    for (int dir = 0; dir < NUM_DIRECTIONS; dir++)
+    {
+        int rev_dir = (dir + NUM_DIRECTIONS / 2) % NUM_DIRECTIONS;
         byte rev_dir_mask = 0x01 << rev_dir;
 
-        int di = directions[i%2][dir][0];
-        int dj = directions[i%2][dir][1];
+        int di = directions[i % 2][dir][0];
+        int dj = directions[i % 2][dir][1];
         int n_i = i + di;
         int n_j = j + dj;
 
-        if (inbounds(n_i, n_j, grid_size)) {
-            if (grid_in[ind2d(n_i,n_j)] == WALL) {
-                next_cell |= from_wall_collision(i, j, grid_in, grid_size, dir);
+        if (inbounds (n_i, n_j, grid_size))
+        {
+            if (grid_in[ind2d (n_i, n_j)] == WALL)
+            {
+                next_cell
+                    |= from_wall_collision (i, j, grid_in, grid_size, dir);
             }
-            else if (grid_in[ind2d(n_i, n_j)] & rev_dir_mask) {
+            else if (grid_in[ind2d (n_i, n_j)] & rev_dir_mask)
+            {
                 next_cell |= rev_dir_mask;
             }
         }
     }
 
-    return check_particles_collision(next_cell);
+    return check_particles_collision (next_cell);
 }
 
-static void update(byte *grid_in, byte *grid_out, int grid_size) {
-    for (int i = 0; i < grid_size; i++) {
-        for (int j = 0; j < grid_size; j++) {
-            if (grid_in[ind2d(i,j)] == WALL)
-                grid_out[ind2d(i,j)] = WALL;
+static void
+update (byte *grid_in, byte *grid_out, int grid_size)
+{
+#pragma omp parallel for
+    for (int i = 0; i < grid_size; i++)
+    {
+        for (int j = 0; j < grid_size; j++)
+        {
+            if (grid_in[ind2d (i, j)] == WALL)
+                grid_out[ind2d (i, j)] = WALL;
             else
-                grid_out[ind2d(i,j)] = get_next_cell(i, j, grid_in, grid_size);
+                grid_out[ind2d (i, j)]
+                    = get_next_cell (i, j, grid_in, grid_size);
         }
     }
 }
 
-void simulate_omp(byte *grid_1, byte *grid_2, int grid_size, int num_threads) {
-    for (int i = 0; i < ITERATIONS/2; i++) {
-        update(grid_1, grid_2, grid_size);
-        update(grid_2, grid_1, grid_size);
+void
+simulate_omp (byte *grid_1, byte *grid_2, int grid_size, int num_threads)
+{
+    // Set some parameters for omp
+    omp_set_dynamic (0);
+    omp_set_num_threads (num_threads);
+
+    for (int i = 0; i < ITERATIONS / 2; i++)
+    {
+        update (grid_1, grid_2, grid_size);
+        update (grid_2, grid_1, grid_size);
     }
 }
